e3695255f82ab46cc8d9aa716582ff4c9a9fc2eb
정지안 <jja6312@naver.com>
2024-10-31 16:43:23 +0900

    delete:jpa

--- src/main/java/com/ssafy/exam/SpringTestApplication.java
+++ src/main/java/com/ssafy/exam/SpringTestApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.ssafy.exam.jpaExample.entity"})
-@EnableJpaRepositories(basePackages = {"com.ssafy.exam.jpaExample.repository"})
 public class SpringTestApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/ssafy/exam/jpaExample/repository/JpaExamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ssafy.exam.jpaExample.repository;
-
-import com.ssafy.exam.jpaExample.entity.JpaEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaExamRepository extends JpaRepository<JpaEntity,Long> {
-}
